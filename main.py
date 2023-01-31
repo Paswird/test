867b1dab3c45004d4826c304719e0b95f34015f6
Paswird <112849427+Paswird@users.noreply.github.com>
2023-01-31 10:44:28 +0100

    hello

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import random
-random.randint(1, 6)
+
 
 a = random.randint(1, 6)
 b = random.randint(1, 6)
 
-print(" у вас випало", a, "і", b )
+r = random.randint(1, 6)
+g = random.randint(1, 6)
 
+tet = a + b
+stb = r + g 
+
+if tet < stb:
+    print("Вася виграв")
+elif tet > stb:
+    print("Гєна ван лав")
+else:
+    print("Два дибіла, то є сила")
